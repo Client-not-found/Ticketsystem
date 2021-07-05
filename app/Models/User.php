9ee2feaa8bd3a9abdaccadd775a069c4c3f42d32
Client-not-found <60361751+Client-not-found@users.noreply.github.com>
2021-07-05 22:18:12 +0200

    Fix Bugs

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable, TwoFactorAuthenticatable;
 
     protected $fillable = ['useKey', 'useGroId', 'useUsername', 'usePassword', 
     'useFirstname', 'useLastname', 'useStreet', 'useZIP', 'useCity', 'useState',
